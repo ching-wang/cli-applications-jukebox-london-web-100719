23a35f954f26cc5083999f053e597b071ab74153
Qing Wang <chingwang.qw@gmail.com>
2019-10-07 20:06:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   songs.each_with_index do |song, index|
-   if song == input || index + 1 == input 
+   if song == input || index + 1 == input.to_i 
       puts "Playing #{song}"
       return
     end 
